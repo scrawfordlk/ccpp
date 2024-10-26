e32eca611795083c099c26bfe431b43a8163b5f9
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-10-26 14:44:01 +0200

    changed formatting rules

--- c/programm.c
+++ c/programm.c
@@ -4,9 +4,7 @@
 int main(int argc, char *argv[]) {
 
     if (argc != 6) {
-        fputs("Usage: programm <personen.dat> <stand.dat> <vorname> <nachname> "
-              "<geburt>\n",
-              stderr);
+        fputs("Usage: programm <personen.dat> <stand.dat> <vorname> <nachname> <geburt>\n", stderr);
         exit(1);
     }
 }
