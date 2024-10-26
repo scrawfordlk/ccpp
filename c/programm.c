30bfc616098f4a13d82e53ce58cde91feb134877
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-10-26 13:40:50 +0200

    created main function with check for argc

---
+++ c/programm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 6) {
+        fputs("Usage: programm <personen.dat> <stand.dat> <vorname> <nachname> "
+              "<geburt>\n",
+              stderr);
+        exit(1);
+    }
+}
