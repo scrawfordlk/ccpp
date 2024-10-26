113cf18581ebba971704b6d1cbcb7a82d6128ea8
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-10-27 00:50:38 +0200

    created person.h with struct and function declarations and completed implementing newId, newPerson, newPeople, freePeople, getWord, readPeople, comparePerson

--- c/person.c
+++ c/person.c
@@ -1,37 +1,98 @@
-/**
- * file that manages everything relating to the person struct
- */
-
-typedef struct Name {
-    char *firstName;
-    char *lastName;
-} Name;
-
-/**
- * defines a Person struct which contains the Name (first and last), birthday and pointers to their
- * parents
- */
-typedef struct Person {
-    Name *name;
-    char *birthday;
-    struct Person *father;
-    struct Person *mother;
-} Person;
-
-/**
- * struct that stores an array of Person (i.e. People). memory allocation is done by
- * malloc(sizeof(People) + sizeof(Person) * len) due to flexible array member
- */
-typedef struct People {
-    int len;
-    Person *list;
-} People;
+#include "person.h"
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * returns a pointer to a People structure of all people in the given file
  */
 People *readPeople(char *fileName) {
-    return 0;
+
+    FILE *filePointer = fopen(fileName, "r");
+    if (filePointer == NULL) {
+        fprintf(stderr, "couldn't open %s", fileName);
+        exit(1);
+    }
+
+    int len = 0;
+    Person **personList = malloc(sizeof(Person *) * len); // dynamic array of Person pointers
+    if (personList == NULL) {
+        exit(1);
+    }
+
+    Id *personId, *fatherId, *motherId;
+    char c, *firstName, *lastName, *birthday;
+    while ((c = getc(filePointer)) != EOF) {
+        ungetc(c, filePointer);
+
+        // person
+        firstName = getWord(filePointer);
+        lastName = getWord(filePointer);
+        getWord(filePointer); // skip gender
+        birthday = getWord(filePointer);
+        personId = newId(firstName, lastName, birthday);
+        getWord(filePointer); // skip year of death
+
+        // father
+        firstName = getWord(filePointer);
+        lastName = getWord(filePointer);
+        birthday = getWord(filePointer);
+        fatherId = newId(firstName, lastName, birthday);
+
+        // mother
+        firstName = getWord(filePointer);
+        lastName = getWord(filePointer);
+        birthday = getWord(filePointer);
+        motherId = newId(firstName, lastName, birthday);
+
+        // resize array and add new Person
+        len++;
+        personList = realloc(personList, sizeof(Person *) * len);
+        if (personList == NULL) {
+            exit(1);
+        }
+        personList[len - 1] = newPerson(personId, fatherId, motherId);
+    }
+
+    fclose(filePointer);
+    return newPeople(len, personList);
+}
+
+/**
+ * returns next word from the part where filePointer is pointing to
+ */
+static char *getWord(FILE *filePointer) {
+    int len = 0;
+    char *word = malloc(sizeof(char) * len);
+
+    char currentChar;
+    while ((currentChar = fgetc(filePointer)) != EOF && currentChar != ' ' && currentChar != '\n') {
+
+        len++;
+        word = realloc(word, sizeof(char) * len);
+        if (word == NULL) {
+            exit(1);
+        }
+
+        word[len - 1] = currentChar;
+        word[len] = '\0';
+    }
+
+    return word;
+}
+
+void printPeople(People *people) {
+    for (int i = 0; i < people->len; i++) {
+        Person *person = people->list[i];
+        printf("%s %s %s %s %s %s %s %s %s\n", person->id->firstName, person->id->lastName,
+               person->id->birthday, person->fatherId->firstName, person->fatherId->lastName,
+               person->fatherId->birthday, person->motherId->firstName, person->motherId->lastName,
+               person->motherId->birthday);
+    }
+}
+
+int main(void) {
+    People *people = readPeople("personen.dat");
+    printPeople(people);
 }
 
 /**
@@ -54,10 +115,87 @@ void sortPeople(People *people) {}
  * where a Person is sorted according to (in order) their birthday, last name, first name
  */
 int comparePerson(Person *personA, Person *personB) {
-    return 0;
+    int cmp = strcmp(personA->id->birthday, personB->id->birthday);
+    if (cmp != 0) {
+        return cmp;
+    }
+
+    cmp = strcmp(personA->id->lastName, personB->id->lastName);
+    if (cmp != 0) {
+        return cmp;
+    }
+
+    return strcmp(personA->id->firstName, personB->id->firstName);
 }
 
 /**
  * swaps pointers of two different Person structs
  */
 void swapPerson(Person **personA, Person **personB) {}
+
+/**
+ * create a new Id and return a pointer to it
+ */
+Id *newId(char *firstName, char *lastName, char *birthday) {
+    Id *id = malloc(sizeof(Id));
+    if (id == NULL) {
+        exit(1);
+    }
+    id->firstName = firstName;
+    id->lastName = lastName;
+    id->birthday = birthday;
+    return id;
+}
+
+/**
+ * created a new Person and return pointer to it
+ */
+Person *newPerson(Id *id, Id *fatherId, Id *motherId) {
+    Person *person = malloc(sizeof(Person));
+    if (person == NULL) {
+        exit(1);
+    }
+    person->id = id;
+    person->fatherId = fatherId;
+    person->motherId = motherId;
+    return person;
+}
+
+/**
+ * create a new People struct and return pointer to it
+ */
+People *newPeople(int len, Person **list) {
+    People *people = malloc(sizeof(People));
+    if (people == NULL) {
+        exit(1);
+    }
+    people->len = len;
+    people->list = list;
+    return people;
+}
+
+/**
+ * frees memory from a People struct
+ */
+void freePeople(People *people) {
+    for (int i = 0; i < people->len; i++) {
+
+        free(people->list[i]->id->firstName);
+        free(people->list[i]->id->lastName);
+        free(people->list[i]->id->birthday);
+        free(people->list[i]->id);
+
+        free(people->list[i]->fatherId->firstName);
+        free(people->list[i]->fatherId->lastName);
+        free(people->list[i]->fatherId->birthday);
+        free(people->list[i]->fatherId);
+
+        free(people->list[i]->motherId->firstName);
+        free(people->list[i]->motherId->lastName);
+        free(people->list[i]->motherId->birthday);
+        free(people->list[i]->motherId);
+
+        free(people->list[i]);
+    }
+    free(people);
+}
---
+++ c/person.h
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// defines a unique identification for a Person
+typedef struct Id {
+    char *firstName;
+    char *lastName;
+    char *birthday;
+} Id;
+
+/**
+ * defines a Person struct that stores its Id (= first name, last name and birthday) as well as the
+ * id's of their father/mother
+ */
+typedef struct Person {
+    Id *id;
+    Id *fatherId;
+    Id *motherId;
+} Person;
+
+/**
+ * struct that stores array of Person pointers and its length
+ */
+typedef struct People {
+    int len;
+    Person **list;
+} People;
+
+Id *newId(char *firstName, char *lastName, char *birthday);
+Person *newPerson(Id *id, Id *fatherId, Id *motherId);
+People *newPeople(int len, Person **list);
+void freePeople(People *people);
+
+static char *getWord(FILE *filePointer);
+People *readPeople(char *fileName);
+
+int comparePerson(Person *personA, Person *personB);
