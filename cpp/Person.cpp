57a686e07a058ffcfeea764d6b3706cf6e57b132
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-12-24 22:02:33 +0100

    split Person.cpp into header and source

--- cpp/Person.cpp
+++ cpp/Person.cpp
@@ -1,77 +1,56 @@
-#include "Identity.cpp"
+#include "Person.h"
 #include <memory>
 #include <ostream>
 
 using std::ostream;
 using std::unique_ptr;
 
-class Person {
-  private:
-    unique_ptr<Identity> self;
-    unique_ptr<Identity> father;
-    unique_ptr<Identity> mother;
-    bool marked;
+Person::Person(unique_ptr<Identity> self, unique_ptr<Identity> father, unique_ptr<Identity> mother)
+    : self(std::move(self)), father(std::move(father)), mother(std::move(mother)), marked(false) {}
 
-  public:
-    // ------------ constructors ----------------
+Person::Person(unique_ptr<Identity> self) : self(std::move(self)) {
+    this->father = nullptr;
+    this->mother = nullptr;
+    this->marked = false;
+}
 
-    Person(unique_ptr<Identity> self, unique_ptr<Identity> father, unique_ptr<Identity> mother)
-        : self(std::move(self)), father(std::move(father)), mother(std::move(mother)),
-          marked(false) {}
+bool Person::isChildOf(const Person &person) const {
+    return (father != nullptr && *father == *person.self) ||
+           (mother != nullptr && *mother == *person.self);
+}
 
-    /*
-     * if this Person has no known parents
-     * */
-    Person(unique_ptr<Identity> self) : self(std::move(self)) {
-        this->father = nullptr;
-        this->mother = nullptr;
-        this->marked = false;
-    }
+bool Person::isParentOf(const Person &person) const {
+    return (person.father != nullptr && *person.father == *self) ||
+           (person.mother != nullptr && *person.mother == *self);
+}
 
-    // --------------- methods -----------------
+void Person::setMark() {
+    marked = true;
+}
 
-    bool isChildOf(const Person &person) const {
-        return (father != nullptr && *father == *person.self) ||
-               (mother != nullptr && *mother == *person.self);
-    }
+bool Person::operator==(const Person &other) const {
+    return *self == *other.self;
+}
 
-    bool isParentOf(const Person &person) const {
-        return (person.father != nullptr && *person.father == *self) ||
-               (person.mother != nullptr && *person.mother == *self);
-    }
+bool Person::operator!=(const Person &other) const {
+    return *self != *other.self;
+}
 
-    void setMark() {
-        marked = true;
-    }
+bool Person::operator<(const Person &other) const {
+    return *self < *other.self;
+}
 
-    // ---------- overloaded operators -------------
+bool Person::operator>(const Person &other) const {
+    return *self > *other.self;
+}
 
-    bool operator==(const Person &other) const {
-        return *self == *other.self;
-    }
+bool Person::operator<=(const Person &other) const {
+    return *self <= *other.self;
+}
 
-    bool operator!=(const Person &other) const {
-        return *self != *other.self;
-    }
-
-    bool operator<(const Person &other) const {
-        return *self < *other.self;
-    }
-
-    bool operator>(const Person &other) const {
-        return *self > *other.self;
-    }
-
-    bool operator<=(const Person &other) const {
-        return *self <= *other.self;
-    }
-
-    bool operator>=(const Person &other) const {
-        return *self >= *other.self;
-    }
-
-    friend ostream &operator<<(ostream &stream, Person &person);
-};
+bool Person::operator>=(const Person &other) const {
+    return *self >= *other.self;
+}
 
 ostream &operator<<(ostream &stream, Person &person) {
     return stream << *person.self;
