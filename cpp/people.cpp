274885861cf7d3428f0d3097e8c84c3e9fdaed6f
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-12-25 18:46:27 +0100

    minor changes for readability

--- cpp/people.cpp
+++ cpp/people.cpp
@@ -7,19 +7,18 @@
 #include <memory>
 #include <string>
 
-using std::array, std::ifstream, std::string, std::unique_ptr, std::make_unique, std::vector;
+using std::array, std::vector, std::string, std::unique_ptr, std::make_unique;
 
 static unique_ptr<Person> constructPerson(const array<string, 11> &personInfo);
 static void sort(unique_ptr<vector<unique_ptr<Person>>> &people);
 
 unique_ptr<vector<unique_ptr<Person>>> readPeople(const char *fileName) {
-    auto people = make_unique<vector<unique_ptr<Person>>>();
-
-    ifstream file = ifstream(fileName);
+    std::ifstream file = std::ifstream(fileName);
     if (!file.is_open()) {
         throw string("Invalid file name: " + string(fileName) + '\n');
     }
 
+    auto people = make_unique<vector<unique_ptr<Person>>>();
     array<string, 11> input;
     while (!file.eof()) {
 
@@ -39,20 +38,23 @@ unique_ptr<vector<unique_ptr<Person>>> readPeople(const char *fileName) {
     return std::move(people);
 }
 
-static unique_ptr<Person> constructPerson(const array<string, 11> &p_info) {
-    auto firstName = make_unique<string>(p_info[0]);
-    auto lastName = make_unique<string>(p_info[1]);
-    auto birthyear = make_unique<string>(p_info[3]);
+/**
+ * returns a unique_ptr to a new Person assembled from the given array of information
+ * */
+static unique_ptr<Person> constructPerson(const array<string, 11> &personFragments) {
+    auto firstName = make_unique<string>(personFragments[0]);
+    auto lastName = make_unique<string>(personFragments[1]);
+    auto birthyear = make_unique<string>(personFragments[3]);
     auto id =
         make_unique<Identity>(std::move(firstName), std::move(lastName), std::move(birthyear));
 
-    auto fFirstName = make_unique<string>(p_info[5]);
-    auto fLastName = make_unique<string>(p_info[6]);
-    auto fBirthyear = make_unique<string>(p_info[7]);
+    auto fFirstName = make_unique<string>(personFragments[5]);
+    auto fLastName = make_unique<string>(personFragments[6]);
+    auto fBirthyear = make_unique<string>(personFragments[7]);
 
-    auto mFirstName = make_unique<string>(p_info[8]);
-    auto mLastName = make_unique<string>(p_info[9]);
-    auto mBirthyear = make_unique<string>(p_info[10]);
+    auto mFirstName = make_unique<string>(personFragments[8]);
+    auto mLastName = make_unique<string>(personFragments[9]);
+    auto mBirthyear = make_unique<string>(personFragments[10]);
 
     // case parents and no parents
     if (*fFirstName != "-" && *mFirstName != "-") {
@@ -66,6 +68,9 @@ static unique_ptr<Person> constructPerson(const array<string, 11> &p_info) {
     }
 }
 
+/**
+ * sorts the vector of unique_ptr's to Person using std::sort()
+ * */
 static void sort(unique_ptr<vector<unique_ptr<Person>>> &people) {
     std::sort(people->begin(), people->end(),
               [](unique_ptr<Person> &p1, unique_ptr<Person> &p2) { return *p1 < *p2; });
