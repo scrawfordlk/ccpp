43e068236a38e897bd6a82e65442c8339aaa17a8
Sam L. K. Crawford <sam.crawford@stud.plus.ac.at>
2024-12-28 21:45:31 +0100

    completed main function

--- cpp/programm.cpp
+++ cpp/programm.cpp
@@ -15,18 +15,22 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    string fileName = argv[1];
+    string inFileName = argv[1];
+    string outFileName = "verwandte.dat";
     string firstName = argv[3];
     string lastName = argv[4];
     string birthyear = argv[5];
 
     unique_ptr<People> people;
+    unique_ptr<People> relatives;
     try {
-        people = readPeople(fileName);
+        people = readPeople(inFileName);
+        relatives = people->getRelatives(firstName, lastName, birthyear);
     } catch (string errorMessage) {
         std::cerr << errorMessage << std::endl;
         return 1;
     }
 
-    std::cout << *people << std::endl;
+    relatives->sort();
+    writePeople(*relatives, outFileName);
 }
